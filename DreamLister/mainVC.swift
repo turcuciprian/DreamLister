1e20ae62f536c05880f316a7a2084ac9b11cf3ba
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-14 02:13:35 +0300

    finished lesson by adding number of sections. Lesson 73 FINISHED

--- DreamLister/mainVC.swift
+++ DreamLister/mainVC.swift
@@ -33,5 +33,8 @@ class mainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
 }
 
