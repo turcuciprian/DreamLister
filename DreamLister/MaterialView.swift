a07808c747868f32fb4356e51f1e13f097b58166
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-14 01:29:19 +0300

    completed the previous commit with the else statement

--- DreamLister/MaterialView.swift
+++ DreamLister/MaterialView.swift
@@ -24,6 +24,11 @@ extension UIView {
                 self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
                 self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor
     
+            }else{
+                self.layer.cornerRadius = 0
+                self.layer.shadowOpacity = 0
+                self.layer.shadowRadius = 0
+                self.layer.shadowcolor = nil
             }
         }
     }
