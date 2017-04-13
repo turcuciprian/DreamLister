d6fad2cd3c75103350fa4a7f1e3a899fec4eea67
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-14 01:43:59 +0300

    hooked up iboutlets to previously viewcell

--- DreamLister/ItemCell.swift
+++ DreamLister/ItemCell.swift
@@ -10,4 +10,10 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
+    
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var details: UILabel!
 }
