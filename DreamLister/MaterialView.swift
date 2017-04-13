123a4766f870fa21dc47ec298b0fcaa082661e19
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-14 01:19:16 +0300

    added a new view named materialview, got rid of the commented code in it

---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,15 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Turcu Ciprian on 14/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    
+
+}
