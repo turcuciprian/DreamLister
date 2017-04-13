d03674d39f73d7cbbd773538437e4822e53eb116
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-14 01:28:02 +0300

    Added properties for the new view shadow

--- DreamLister/MaterialView.swift
+++ DreamLister/MaterialView.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class MaterialView: UIView {
+private var materialKey = false
+extension UIView {
+    
+    @IBInspectable var materialDesign: Bool{
+        get{
+            return materialKey
+        } set{
+            materialKey = newValue
+            if materialKey {
+                self.layer.masksToBounds = false
+                self.layer.cornerRadius = 3.0
+                self.layer.shadowOpacity = 0.8
+                self.layer.shadowRadius = 3.0
+                self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor
+    
+            }
+        }
+    }
 
     
 
