b51f99e0f35b9816d81cb653c766413db4f3115a
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-14 18:45:07 +0300

    added a controller function for when a object changed and we have the insert, delete, update functionality - managing that

--- DreamLister/mainVC.swift
+++ DreamLister/mainVC.swift
@@ -57,8 +57,18 @@ class mainVC: UIViewController, UITableViewDelegate, UITableViewDataSource,NSFet
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
     }
+    
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            <#code#>
+        default:
+            <#code#>
+        }
     }
 }
 
